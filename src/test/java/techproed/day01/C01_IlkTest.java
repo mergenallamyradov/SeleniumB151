edad203716efe961f51c516c3fd6f4343dd646eb
Mergen Allamyradov <ma.mergen@gmail.com>
2023-05-27 01:38:47 +0500

    ready

--- src/test/java/techproed/day01/C01_IlkTest.java
+++ src/test/java/techproed/day01/C01_IlkTest.java
@@ -1,2 +1,17 @@
-package techproed.day01;public class C01_IlkTest {
+package techproed.day01;
+
+import  org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chromeDriver", "src/resources/drivers/chromedriver.exe");
+
+        System.out.println(System.getProperty("webdriver.chromeDriver"));
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:/techproeducation.com");
+    }
+
 }
+
--- src/test/java/techproed/day01/C02_DriverMethods.java
+++ src/test/java/techproed/day01/C02_DriverMethods.java
@@ -1,2 +1,31 @@
-package techproed.day01;public class C02_DriverMethods {
+package techproed.day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_DriverMethods {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://amazon.com");
+        System.out.println("Amazon Sayfa basligi : " + driver.getTitle());
+        System.out.println("Amazon Actual Url : "+driver.getCurrentUrl());
+
+
+        driver.get("https://techproeducation.com");
+        System.out.println("Techproeducation Sayfa Basligi : " + driver.getTitle()); // Sayfa basligi verir
+
+        //getCurrentUrl() Gidilen sayfanın Url'ini verir
+        System.out.println("Techproed Actual Url : "+driver.getCurrentUrl());
+
+        //getPageSource() Açılan sayfanın kaynak kodlarını verir
+        System.out.println(driver.getPageSource()); //Bilerek yoruma aldık her çalıştırdığımızda konsola bütün kaynak kodlarını vereceği için
+
+        //getWindowHandle() Gidilen sayfanın handle değerini(hashKod) verir. Bu handle değerini sayfalar arası geçiş için kullanırız
+        System.out.println("Techproed Window Handle Değeri : "+driver.getWindowHandle());//156B7B83BB29C993FACBBE3CBBFDFD5
+
+
+
+    }
 }
