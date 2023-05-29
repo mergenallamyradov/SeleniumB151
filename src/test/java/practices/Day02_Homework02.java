7aa3950f825d06cebdece6af1374dbd2fa58084b
Mergen Allamyradov <ma.mergen@gmail.com>
2023-05-29 07:53:50 +0500

    29/05/2023

---
+++ src/test/java/practices/Day02_Homework02.java
@@ -0,0 +1,18 @@
+package practices;
+
+public class Day02_Homework02 {
+    /*
+    Yeni bir class olusturun (TekrarTesti)
+Youtube web sayfasına gidin ve sayfa başlığının “youtube” olup olmadığını doğrulayın  (verify), eğer değilse doğru başlığı(Actual Title) konsolda yazdirin.
+Sayfa URL'sinin “youtube” içerip içermediğini (contains) doğrulayın, içermiyorsa doğru  URL'yi yazdırın.
+Daha sonra Amazon sayfasina gidin https://www.amazon.com/
+Youtube sayfasina geri donun
+Sayfayi yenileyin
+Amazon sayfasina donun
+Sayfayi tamsayfa yapin
+Ardından sayfa başlığının "Amazon" içerip içermediğini (contains) doğrulayın, Yoksa  doğru başlığı(Actual Title) yazdırın.
+Sayfa URL'sinin https://www.amazon.com/ olup olmadığını doğrulayın, degilse doğru
+URL'yi yazdırın
+Sayfayi kapatin
+     */
+}
